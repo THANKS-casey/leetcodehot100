8ff1ccbf6b76c685e44226b52b630bd324cb090a
Cathy <1669676009@qq.com>
2025-02-28 11:15:41 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1,n):# 当我们使用遍历整个数组的方式寻找 target - x 时，需要注意到每一个位于 x 之前的元素都已经和 x 匹配过，因此不需要再进行匹配
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+
+        return [] # delete this will cause nothing
