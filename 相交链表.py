378bd16b2e7f0a35e4e49d868193bc3063d361a6
Cathy <1669676009@qq.com>
2025-03-01 09:42:36 +0800

    Update 相交链表.py

--- 相交链表.py
+++ 相交链表.py
@@ -1,7 +1,7 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         A, B = headA, headB
-        while A != B:
+        while A!= B: #其实是判断两个节点是否是一个节点 所以其实就一个A == B的逻辑，而不是A.val == B.val(即除了值相等，还要地址相同)
             A = A.next if A else headB
             B = B.next if B else headA
-        return A #如果相交，返回的就是一条非空链表，如果不相交，返回的就是null（链表的末端）
+        return A
