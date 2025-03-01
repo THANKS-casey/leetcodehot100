10429c92d9083189a862b9bbd98bb7ccac893b6e
Cathy <1669676009@qq.com>
2025-03-01 10:55:18 +0800

    Update 回文链表.py

--- 回文链表.py
+++ 回文链表.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        vals = []
+        vals = [] #变量名别用vars，vars跟查属性的内置函数vars()重复
         A = head
         while A is not None:
             vals.append(A.val)
