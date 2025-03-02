00d877b0b3a250146b0d9bec9e5b4b19f69cc37c
Cathy <1669676009@qq.com>
2025-03-02 16:19:32 +0800

    Update 环形链表.py

--- 环形链表.py
+++ 环形链表.py
@@ -1,9 +1,10 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        seen = set()
-        while head:
-            if head in seen:
+        seen = set() #这里是未来判断重不重复，所以用集合而不是列表（）
+        A = head
+        while A is not None:
+            if A in seen:
                 return True
-            seen.add(head)
-            head = head.next
+            seen.add(A) #这里集合加元素是用add 列表加才是用append
+            A = A.next
         return False
